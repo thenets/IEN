ab0a4f78c43673b06a68e91abdb432aacabdf5eb
Luiz Felipe <phelip@thenets.org>
2014-11-27 22:37:01 +0000

    Finished login system

--- application/config/database.php
+++ application/config/database.php
@@ -48,9 +48,9 @@
 $active_group = 'default';
 $active_record = TRUE;
 
-$db['default']['hostname'] = 'localhost';
+$db['default']['hostname'] = '127.0.0.1';
 $db['default']['username'] = 'thenets';
-$db['default']['password'] = 'ien';
+$db['default']['password'] = '';
 $db['default']['database'] = 'ien_main';
 $db['default']['dbdriver'] = 'mysql';
 $db['default']['dbprefix'] = '';
--- application/models/user.php
+++ application/models/user.php
@@ -1,23 +1,21 @@
 <?php
-Class User extends CI_Model
-{
- FUNCTION login($username, $password)
- {
-   $this -> db -> select('id, username, PASSWORD');
+class User extends CI_Model {
+ function login($username, $password) {
+   $this -> db -> select('id, username, password');
    $this -> db -> from('users');
-   $this -> db -> where('USERNAME', $username);
+   $this -> db -> where('username', $username);
    $this -> db -> where('password', MD5($password));
    $this -> db -> limit(1);
  
    $query = $this -> db -> get();
  
-   if($query -> num_rows() == 1)
-   {
-     RETURN $query->result();
+   if($query -> num_rows() == 1) {
+     return $query->result();
    }
-   else
-   {
+   else {
      return false;
    }
  }
+ 
+ 
 }
